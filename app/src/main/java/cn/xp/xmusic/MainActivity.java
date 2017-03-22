ac35fd875e12ab22814e02d5f3f6fd6440007e60
xp664221812 <realxp141414@163.com>
2017-03-22 20:09:49 +0800

    commit 3

--- app/src/main/java/cn/xp/xmusic/MainActivity.java
+++ app/src/main/java/cn/xp/xmusic/MainActivity.java
@@ -2,11 +2,16 @@ package cn.xp.xmusic;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
 
 import com.jude.beam.bijection.BeamAppCompatActivity;
 
 public class MainActivity extends BeamAppCompatActivity {
     private DrawerLayout mDrawerLayout;
+    boolean drawerOpened = false;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,35 @@ public class MainActivity extends BeamAppCompatActivity {
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
+
     }
+
+
+    private DrawerLayout.DrawerListener mDrawerListener = new DrawerLayout.DrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View arg0) {
+            drawerOpened = false;
+
+        }
+
+        @Override
+        public void onDrawerOpened(View view) {
+
+        }
+
+        @Override
+        public void onDrawerSlide(View arg0, float arg1) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int arg0) {
+
+        }
+
+    };
 }
